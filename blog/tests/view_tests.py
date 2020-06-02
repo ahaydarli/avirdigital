6e94c0365bb25d4e6c509ed7a8b1bfa087bfaa64
Ali <5868653+ahaydarli@users.noreply.github.com>
2020-06-02 15:50:45 +0400

    Update view_tests.py

--- blog/tests/view_tests.py
+++ blog/tests/view_tests.py
@@ -6,7 +6,7 @@ from avirdigital import settings
 class BlogTest(TestCase):
     def test_index(self):
         response = {}
-        response.status_code = 200
+        response['status_code'] = 200
         self.assertEqual(response.status_code, 200)
 
     def test_view(self):
